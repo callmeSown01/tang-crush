892e57653dbd3f53f7fc8709bbf22dc65a63147b
callmeSown01 <88945886+callmeSown01@users.noreply.github.com>
2021-08-15 02:29:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Cho anh câu trả lời vì sao em lại đồng ý được không? <3',
     btnReply: 'Gửi cho anh <3',
-    reply: '',
+    reply:  '',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'hjhj.',
     btnAccept: 'Okiiiii lun <3',
